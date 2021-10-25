fa3048f78e4706ba5a3ae147f46b49b0b143a712
matt <helllomatt@users.noreply.github.com>
2021-10-25 13:56:38 -0500

    declare express extensions for TS

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,17 @@
-import { Renderer, RendererOptions } from './renderer';
+import { Renderer, RendererOptions, RendererOptionsOverride, CompilationOptions } from './renderer';
 
 export {
     Renderer, RendererOptions, RendererOptionsOverride, CompilationOptions, WebpackOptions, EntryFiles } from './renderer';
 
 export const CreateRenderer = (options: RendererOptions): Renderer => {
     return new Renderer(options);
+}
+
+declare global {
+    namespace Express {
+        export interface Response {
+            vue(file: string, context?: any, options?: RendererOptionsOverride): void;
+            vueConfig(file: string, context?: any, options?: RendererOptionsOverride): CompilationOptions;
+        }
+    }
 }
